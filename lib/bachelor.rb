bc83d0f607a3066bde25e8c6a6e8ed9cf70767c5
github username <github email address>
2018-06-01 15:39:18 +0100

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,60 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  names = []
+  data.each do |i_season, info|
+    info.each do |ladies|
+      if ladies["status"] == "Winner" && i_season == season
+        names.push(ladies["name"].split)
+      end
+    end
+  end
+  names.flatten[0]# code here
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do|i_season, info|
+      info.each do |ladies|
+        if ladies["occupation"] == occupation
+            return ladies["name"]
+        end
+      end
+  end# code here
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.each do |i_season, info|
+    info.each do |ladies|
+      if ladies["hometown"] == hometown
+        i += 1
+      end
+    end
+  end
+  i# code here
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |i_season, info|
+    info.each do |ladies|
+      if ladies["hometown"] == hometown
+        return ladies["occupation"]
+        break
+      end
+    end
+  end# code here
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  i = 0
+  age = 0
+  data.each do |i_season, info|
+    info.each do |ladies|
+      if season == i_season
+        age = ladies["age"].to_f + age
+        i += 1
+      end
+    end
+  end
+  average = age / i
+  average.round
+   # code here
 end
